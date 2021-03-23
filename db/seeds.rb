119f5907863789a40a9089150dbced634c40d578
Ian Davies <mr.daviesian@gmail.com>
2021-03-22 20:08:47 -0500

    commit tree c109df258cc6b6e17460e7eb30249326f47132d0

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Song.destroy_all
+
+Artist.create(name: "Ian Davies")
+Artist.create(name: "Nai Seivad")
+Song.create(title: "Schwaazy", artist_id: Artist.first.id)
+Song.create(title: "Un_Schwaasty", artist_id: Artist.second.id)
